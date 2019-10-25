098d903ce9474bceb10c7f4084c635e3f04aa725
Angelin Calu <angelin.calu@gmail.com>
2019-10-25 22:19:08 +0300

    fixed to function

--- src/extension.ts
+++ src/extension.ts
@@ -28,11 +28,11 @@ export function activate(context: vscode.ExtensionContext) {
     let RegisterGeneralMethods = vscode.languages.registerCompletionItemProvider('pml', {
 
         provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) {
-          
+
             const filteredGeneralMethods = dictionary.filter(methods => methods.library === "General");
             let Methods = (filteredGeneralMethods[0].methods).map(method => {
 
-                let item =  new vscode.CompletionItem(method.label, vscode.CompletionItemKind.Method);
+                let item = new vscode.CompletionItem(method.label, vscode.CompletionItemKind.Method);
 
                 if (method.snippet) {
                     item.insertText = new vscode.SnippetString(method.snippet);
@@ -41,19 +41,19 @@ export function activate(context: vscode.ExtensionContext) {
                 if (method.md) {
                     item.documentation = new vscode.MarkdownString(method.md);
                 }
-                
+
                 return item;
 
             });
 
             return Methods;
         }
-        
+
     });
-   
-    context.subscriptions.push( RegisterKeywords, RegisterGeneralMethods, Uglifier);
+
+    context.subscriptions.push(RegisterKeywords, RegisterGeneralMethods, Uglifier);
     context.subscriptions.push(vscode.languages.registerDocumentSymbolProvider(
-        {language: "pml"}, new PmlDocumentSymbolProvider()
+        { language: "pml" }, new PmlDocumentSymbolProvider()
     ));
 
     vscode.workspace.onDidChangeTextDocument(onDidChangeTextDocument);
@@ -97,18 +97,18 @@ export class PmlDocumentSymbolProvider implements vscode.DocumentSymbolProvider 
 
 function onDidChangeTextDocument(e: vscode.TextDocumentChangeEvent) {
     if (!vscode.window.activeTextEditor) {
-		return; // no editor
+        return; // no editor
     }
-    
-	let {
-		document
-	} = vscode.window.activeTextEditor;
 
-	var lines = document.lineCount;
-    
-    var varString: {name: string, type: string, from: Number, to: Number | null, global: Boolean};
+    let {
+        document
+    } = vscode.window.activeTextEditor;
+
+    var lines = document.lineCount;
+
+    var varString: { name: string, type: string, from: Number, to: Number | null, global: Boolean };
     var variables: any[] = [];
-    
+
     for (let l = 0; l < lines; l++) {
         var lineContent = document.lineAt(l).text
 
@@ -122,85 +122,85 @@ function onDidChangeTextDocument(e: vscode.TextDocumentChangeEvent) {
             var from = 0;
             var global;
 
-            // if (lineContent.toLowerCase().startsWith('define ')) {
-            //     // set all empty to to: l-1
-            //     let OpenVars = variables.filter(variable => (variable.name !== varString.name && variable.to !== null));
-                
-            //     (OpenVars[0]).name.map((variable: { to: number; }) => {
-            //         variable.to = l-1;
+            if (lineContent.toLowerCase().startsWith('define ') || lineContent.toLowerCase().startsWith('endmethod')) {
 
-            //         return variable;
-            //     }
-                
-            // }
-            
+                variables.forEach(function (variable) {
+
+                    if (variable.to === null) {
+                        variable.to = l - 1;
+                    }
+
+                });
+
+            }
+
             while (match = regex.exec(lineContent)) {
                 if (match && match[1] != "this") {
-    
+
                     var to = null;
 
                     if (lineContent.toLowerCase().startsWith('setup form')) {
                         type = "Form";
                         from = l;
                     }
-    
+
                     if (lineContent.toLowerCase().includes(' = object marui()')) {
                         type = "MarUi";
                         from = l;
                     }
-                    
+
                     if (lineContent.toLowerCase().includes(' = object marutil()')) {
                         type = "MarUtil";
                         from = l;
                     }
-                    
+
                     if (lineContent.toLowerCase().includes(' = object mardrafting()')) {
                         type = "MarDrafting";
                         from = l;
                     }
-                    
+
                     if (lineContent.toLowerCase().includes(' = object marelementhandle()')) {
                         type = "MarElementHandle";
                         from = l;
                     }
-                    
+
                     if (lineContent.toLowerCase().includes(' = object marmodel()')) {
                         type = "MarModel";
                         from = l;
                     }
-                    
+
                     if (lineContent.toLowerCase().includes(' = object marpanelschema()')) {
                         type = "MarPanelSchema";
                         from = l;
                     }
-                    
+
                     if (lineContent.toLowerCase().includes(' = object marhullpan()')) {
                         type = "MarHullPan";
                         from = l;
                     }
-    
+
                     if (match[1].startsWith('!!')) {
                         global = true;
-                    }else {
-                        global = false;
                         to = lines;
+                    } else {
+                        global = false;
                     }
-                    
-    
+
+
                     varString = {
                         name: match[1],
                         type: type,
                         from: from,
                         to: to,
                         global: global
-                    };    
-                    
+                    };
+
                     if (variables.filter(variable => (variable.name !== varString.name && variable.to !== null))) {
                         variables.push(varString);
                     }
-                    
+
                 }
-            
+
             }
 
         }
